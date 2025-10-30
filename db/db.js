6cc80a31bb1a56714ccd54e6ce3a3b3e4ad575bb
Ramsey99 <adhikarianuradha336@gmail.com>
2025-10-30 06:33:39 +0530

    update db

--- db/db.js
+++ db/db.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 const DBURL = process.env.ATLAS_DBURL;
+require("dotenv").config();
 
 mongoose
-  .connect(DBURL)
+  .connect(DBURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
